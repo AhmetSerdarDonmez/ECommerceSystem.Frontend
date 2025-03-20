aa9d85adf9311fae4defd1c3eeb2210318db3e3f
Ahmet Serdar Dönmez <a.serdar2002@gmail.com>
2025-03-20 12:38:40 +0300

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,14 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 50619,
+ //       https: true, // This line will make your Vite dev server run on HTTPS
         proxy: {
             '/api': {
                 target: 'https://localhost:7196',
                 changeOrigin: true,
-                secure: false,
-//                rewrite: (path) => path.replace(/^\/api/, '')
+                secure: false, // Keep this if your backend uses a self-signed certificate locally
+                // https: true, // Remove this line, it's for the proxy to the backend
+                // rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
     }
